4fae6a13cafeae66567029d6fd1ec74b68a41e96
James Michie <jamesmichie@Jamess-MacBook-Pro.local>
2024-08-21 23:03:45 +0100

    Completed initial work on code refactoring

--- fpl_data_retrieval.py
+++ fpl_data_retrieval.py
@@ -2,203 +2,228 @@ import pandas as pd
 import requests
 import json
 import os
-import Functions.fpl_functions as fpl
+from datetime import datetime
+import functions.fpl_functions as fpl
+
 
 print("---------- SCRIPT STARTED ----------")
 
-##### READING CONFIG FILE #####
+
+# Retrieve general information about the FPL season from the API
+general_fpl_info = requests.get('https://fantasy.premierleague.com/api/bootstrap-static/')
+api_return_code = str(general_fpl_info)[-5:-2]
+
+if api_return_code != '200':
+
+    print(f"General Info API request failed, return code: {api_return_code}")
+    print("********** SCRIPT ENDED ON ERROR **********")
+    exit()
+
+else:
+
+    print("General Info API request successful")
+    general_fpl_info = general_fpl_info.json()
+
+    pass
+
+del api_return_code
+
+
+
+# Determine the current season
+season_start_date_str = general_fpl_info['events'][0]['deadline_time']
+season_start_year = datetime.strptime(season_start_date_str, '%Y-%m-%dT%H:%M:%SZ').year
+season_end_year = season_start_year + 1
+
+current_season = f'{season_start_year}-{str(season_end_year)[-2:]}'
+
+del season_start_date_str, season_start_year, season_end_year
+
+
+
+# Generate file paths required for the script
+(
+    CONFIG_JSON_FILEPATH,
+    GAMEWEEK_FILES_DIRECTORY
+)= fpl.pathfinder(season= current_season)
+
+
+
+# Read in config file
 try:
-    with open(os.path.join(os.path.dirname(__file__), "Configuration", "fpl_config.json")) as config_file:
-        config = json.load(config_file)
+
+    with open(CONFIG_JSON_FILEPATH) as temporary_file:
+        config = json.load(temporary_file)
 
 except Exception as e:
+
     print(f"Error encountered while reading in config file: {e}")
     print("********** SCRIPT ENDED ON ERROR **********")
     exit()
 
 
 
-##### RETRIEVING CONFIG DATA #####
-# Calling API endpoint containing general information on the current season.
-general_fpl_info = requests.get('https://fantasy.premierleague.com/api/bootstrap-static/')
-api_return_code = str(general_fpl_info)[-5:-2]
+# Determine the current gameweek
+print('Checking which FPL gameweek has been most recently completed...')
+no_gameweeks_completed_yet = True
 
-if api_return_code == "200":
+for gameweek in general_fpl_info['events']:
 
-    print("General Info API request successful")
+    if not gameweek['finished']:
 
-    # Determining how many football players are in the FPL database.
-    general_fpl_info = general_fpl_info.json()
-    no_of_players = len(general_fpl_info['elements'])
+        no_gameweeks_completed_yet = False
 
-else:
-    print(f"General Info API request failed, return code: {api_return_code}")
-    print("********** SCRIPT ENDED ON ERROR **********")
-    exit()
+        current_gameweek = gameweek['name']
+        current_gameweek_number = int(current_gameweek[-2:])
+        last_completed_gameweek_number = current_gameweek_number - 1
+        last_completed_gameweek_string = f'Gameweek {last_completed_gameweek_number}'
 
+        print(f'{last_completed_gameweek_string} is the most recently completed gameweek.')
 
-# Calling API endpoint containing player data for previous gameweeks.
-previous_gameweeks_data = requests.get('https://fantasy.premierleague.com/api/element-summary/1')
-api_return_code = str(previous_gameweeks_data)[-5:-2]
+        break
 
-if api_return_code == "200":
-
-    print("Previous Gameweeks API request successful")
-    
-    # Determining how many gameweeks have been played of the current FPL season.
-    previous_gameweeks_data = previous_gameweeks_data.json()
-    no_of_gameweeks = len(previous_gameweeks_data['history'])
-
-else:
-    print(f"Previous Gameweeks API request failed, return code: {api_return_code}")
-    print("********** SCRIPT ENDED ON ERROR **********")
-    exit()
-
-
-# Converting the general info API return to a dataframe and adding some columns ahead of a merge later on in the script.
-fpl_df = pd.json_normalize(general_fpl_info['elements'])
-fpl_df["full_name"] = fpl_df["first_name"] + " " + fpl_df["second_name"]
-
-fpl_df["team_name"] = fpl_df["team"].apply(lambda row: fpl.convert_to_string(str(row), 
-                                                                     config_category= "team_code", 
-                                                                     config_file= config))
-
-fpl_df["position"] = fpl_df["element_type"].apply(lambda row: fpl.convert_to_string(str(row), 
-                                                                            config_category= "position_code", 
-                                                                            config_file= config))
-
-
-
-##### RETRIEVING PLAYER DATA #####
-# Making an API call to retrieve data for each player and appending the data to a list of dictionaries.
-print("Starting extraction of individual player data...")
-
-player_data_dictionaries = []
-for player in range(no_of_players + 1):
-
-    player_data = requests.get(f"https://fantasy.premierleague.com/api/element-summary/{player}/")
-    api_return_code = int(str(player_data)[-5:-2])
-
-    if api_return_code != 200:
+    else:
         continue
 
-    player_data = player_data.json()
-    player_data_dictionaries.append(player_data["history"])
+if no_gameweeks_completed_yet:
 
-    if player % 100 == 0 and player != 0:
-        print(f"Data for {player} players extracted...")
-
-print("Extraction of player data complete.")
-
-
-
-##### TRANSFORMING DATA AND LOADING INTO DATABASES #####
-# For loop which generates a database for each gameweek played, containing data for each player's performance in that gameweek.
-print("Starting database creation for each gameweek...")
-
-no_of_player_dictionaries = len(player_data_dictionaries)
-for gameweek in range(no_of_gameweeks):
-
-    print(f"Creating database for gameweek {gameweek + 1}...")
-
-    player_dataframes = []
-    double_gameweek = False
-
-    for player_id in range(no_of_player_dictionaries):
-
-        first_dictionary = player_data_dictionaries[player_id][0]
-        second_dictionary = player_data_dictionaries[player_id][1] if len(player_data_dictionaries[player_id]) >= 2 else None
-
-        if first_dictionary["round"] == gameweek + 1 and (second_dictionary and second_dictionary["round"] == gameweek + 1):
-            double_gameweek = True
-
-        if double_gameweek:
-            
-            # Combining data from the two matches in the double gameweek.
-            player_df = fpl.double_gameweek_processing(first_dictionary, second_dictionary, config)
-
-            player_dataframes.append(player_df)
-
-            player_data_dictionaries[player_id].pop(0)
-            player_data_dictionaries[player_id].pop(0)
-
-            double_gameweek = False
-            second_dictionary = None
-        
-        elif first_dictionary["round"] == gameweek + 1:
-
-            player_df = pd.json_normalize(first_dictionary)
-            player_dataframes.append(player_df)
-            
-            player_data_dictionaries[player_id].pop(0)
-
-        else:
-            continue
-        
-
-    gameweek_data = pd.concat(player_dataframes)
-
-    gameweek_data = gameweek_data.rename(columns= {"element" : "id"})
-    gameweek_data = gameweek_data.astype(config["previous_gameweeks_config"]["before_calculations"])
-
-    # Merging our dataframe with selected columns from the 'general_info' dataframe.
-    general_info_columns = fpl_df[["full_name", "team_name", "position", "now_cost", "id"]]
-    merged_df = pd.merge(gameweek_data, general_info_columns, how= "right", on= "id")
-
-    # Adding some columns to the dataframe containing calculations of a players performance against selected metrics.
-    fpl.performance_metric_calculations(dataframe= merged_df)
-
-    # Cleaning the database and writing it to a csv.
-    column_order = list(config["previous_gameweeks_config"]["after_calculations"].keys())
-    merged_df = merged_df[column_order]
-    merged_df = merged_df.fillna(config["previous_gameweeks_config"]["na_columns"])
-    merged_df = merged_df.astype(config["previous_gameweeks_config"]["after_calculations"])
-
-    merged_df.to_csv(os.path.join(os.path.dirname(__file__), "Databases", f"GW{gameweek + 1}.csv"), index= False)
-    print("Database successfully created.")
-
-
-
-##### RETIRIEVING LIVE GW DATA #####
-# Repeating the process performed in the above block, but for the most recent gameweek.
-current_gameweek = no_of_gameweeks + 1
-
-if current_gameweek > 38:
-    
+    print('No gameweeks have been completed yet')
     print("---------- SCRIPT COMPLETED ----------")
     exit()
 
 else:
     pass
 
-current_gameweek_data = requests.get(f"https://fantasy.premierleague.com/api/event/{current_gameweek}/live/")
+del gameweek, current_gameweek
 
-current_gameweek_data_dict = current_gameweek_data.json()
 
-print(f"Creating database for gameweek {current_gameweek}...")
 
-player_dataframes = []
-for player in range(no_of_players):
+# Determine which gameweeks need to be processed
+missing_gameweeks_list = [
+    x for x in range(1, current_gameweek_number) 
+    if f'Gameweek_{x}.csv' not in os.listdir(GAMEWEEK_FILES_DIRECTORY)
+]
 
-    player_df = pd.json_normalize(current_gameweek_data_dict['elements'][player]['stats'])
-    player_df['id'] = current_gameweek_data_dict['elements'][player]['id']
+if not missing_gameweeks_list:
 
-    player_dataframes.append(player_df)
+    print('Files have already been generated for all completed gameweeks')
+    print("---------- SCRIPT COMPLETED ----------")
+    exit()
 
-gameweek_data = pd.concat(player_dataframes)
-gameweek_data = gameweek_data.astype(config["current_gameweek_config"]["before_calculations"])
+else:
+    pass
 
-merged_df = pd.merge(gameweek_data, general_info_columns, how="right", on= "id")
 
-fpl.performance_metric_calculations(dataframe= merged_df)
 
-column_order = list(config["previous_gameweeks_config"]["after_calculations"].keys())
-merged_df = merged_df[column_order]
-merged_df = merged_df.fillna(config["current_gameweek_config"]["na_columns"])
-merged_df = merged_df.astype(config["current_gameweek_config"]["after_calculations"])
+# Retrieve player data for the required gameweeks from the API
+gameweek_dictionary_list = []
 
-merged_df.to_csv(os.path.join(os.path.dirname(__file__), "Databases", f"GW{current_gameweek}.csv"), index= False)
+for gameweek_number in missing_gameweeks_list:
+
+    api_endpoint_url = f"https://fantasy.premierleague.com/api/event/{gameweek_number}/live/"
+    gameweek_data = requests.get(api_endpoint_url)
+    api_return_code = str(gameweek_data)[-5:-2]
+
+    if api_return_code != '200':
+
+        ### I can probably raise an exception here and continue with the script
+
+        print(f'API call for last completed gameweek unsuccessful. Return code: {api_return_code}')
+        print("********** SCRIPT ENDED ON ERROR **********")
+        exit(1)
+
+    else:
+
+        gameweek_dict = gameweek_data.json()
+        # gameweek_dict[f'gameweek_{gameweek_number}'] = gameweek_dict.pop('elements')
+        gameweek_dictionary_list.append(gameweek_dict)
+
+    del api_return_code, api_endpoint_url, gameweek_data, gameweek_dict
+
+
+
+# Retrieve player details from the general_fpl_info dictionary, ahead of a dataframe join at the end of gameweek processing
+player_details_df = pd.json_normalize(general_fpl_info['elements'])
+player_details_df = player_details_df[config['player_details_columns_list']]
+player_details_df['full_name'] = player_details_df['first_name'] + ' ' + player_details_df['second_name']
+
+player_details_df = player_details_df.drop(
+    labels= ['first_name', 'second_name'],
+    axis= 1
+)
+
+player_details_df = fpl.map_column_values_to_string(
+    general_info_dict= general_fpl_info,
+    dataframe= player_details_df
+)
+
+
+# Convert gameweek dictionary into dataframe and merge with player details
+for gameweek_dict, gameweek in zip(gameweek_dictionary_list, missing_gameweeks_list):
+
+    player_dataframe_list = []
+
+    for player in range(0, len(gameweek_dict['elements'])):
+
+        player_id = gameweek_dict['elements'][player]['id']
+        player_data_df = pd.json_normalize(gameweek_dict['elements'][player]['stats'])
+        player_data_df['id'] = player_id
+
+        player_dataframe_list.append(player_data_df)
+
+    full_gameweek_df = pd.concat(player_dataframe_list)
+    full_gameweek_df = full_gameweek_df.merge(
+        right= player_details_df,
+        how= 'inner',
+        on= 'id'
+    )
+
+    # Clean dataframe and write to csv
+    full_gameweek_df = full_gameweek_df.drop(
+        labels=['bonus', 'bps', 'in_dreamteam', 'yellow_cards', 'red_cards', 'own_goals'], 
+        axis= 1
+    )
+
+    full_gameweek_df = full_gameweek_df.astype(config['column_dtypes_mapper'])
+    full_gameweek_df['now_cost'] = full_gameweek_df['now_cost'] / 10
+    full_gameweek_df = fpl.xpoints_calculation(full_gameweek_df)
+    full_gameweek_df = full_gameweek_df[config['column_reordering_list']]
+
+    csv_filepath = os.path.join(
+        os.path.dirname(__file__), 
+        'player_data', 
+        current_season, 
+        f'Gameweek_{gameweek}.csv'
+    )
+
+    full_gameweek_df.to_csv(
+        path_or_buf= csv_filepath,
+        index= False
+    )
+
+    del player_id, player_data_df
+
+### Double gameweeks will likely break this for loop, but I don't know exactly how, will need to revisit later in the season
 
 print("Database successfully created.")
-
 print("---------- SCRIPT COMPLETED ----------")
+
+
+# # Calculations I've removed from the datbases (manually set up in Tableau)
+# dataframe["xpoints_to_cost_ratio"] = dataframe["xpoints"] / dataframe["now_cost"]
+
+# dataframe["goal_involvements"] = dataframe["goals_scored"] + dataframe["assists"]
+# dataframe["90s"] = dataframe["minutes"] / 90
+# dataframe["expected_goals_per_90"] = dataframe["expected_goals"] / dataframe["90s"]
+# dataframe["expected_assists_per_90"] = dataframe["expected_assists"] / dataframe["90s"]
+# dataframe["expected_goal_involvements"] = dataframe["expected_goals"] + dataframe["expected_assists"]
+# dataframe["expected_goal_involvements_per_90"] = dataframe["expected_goal_involvements"] / dataframe["90s"]
+# dataframe["goals_conceded_per_90"] = dataframe["goals_conceded"] / dataframe["90s"]
+# dataframe["expected_goals_conceded_per_90"] = dataframe["expected_goals_conceded"] / dataframe["90s"]
+# dataframe["saves_per_90"] = dataframe["saves"] / dataframe["90s"]
+# dataframe["clean_sheets_per_90"] = dataframe["clean_sheets"] / dataframe["90s"]
+# dataframe["xgi_to_cost_ratio"] = (dataframe["expected_goal_involvements"] * 100) / dataframe["now_cost"]
+
+# dataframe["performance_vs_xg"] = dataframe["goals_scored"] - dataframe["expected_goals"]
+# dataframe["performance_vs_xa"] = dataframe["assists"] - dataframe["expected_assists"]
+# dataframe["performance_vs_xgc"] = dataframe["expected_goals_conceded"] - dataframe["goals_conceded"]
